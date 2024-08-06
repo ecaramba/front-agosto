ff5ffc65c356043350235c143dae53cf3446c2d5
Prof Edir <ecaramba@gmail.com>
2024-08-05 21:42:43 -0300

    javascript

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// comentario de uma linha
+/* comentario
+ de varias
+ linhas
+ */
+// javascript não é Java
+
+// var nome = prompt("Digite seu nome");
+
+// alert("Olá " + nome + "!!!");
+
+var nome = "Edir"; //texto -> string
+var idade = 23; // number -> integer
+var peso = 34.6; // number -> float
+var acordado = true; // boolean  
+
+// exercicios
+// 1 - diga quantos caracteres tem na palavra informada
+
+// 1 - pegar a palavra digitado
+var palavra = prompt("Digite uma palavra");
+// 2 - contar os caracteres
+var quant = palavra.length;
+// 3 - exibir o resultado
+alert("a quantidade de caracteres digitadas é: " + quant);
