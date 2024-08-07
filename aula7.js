03f4a636019f9d2f271bd2d6703c4fb74fca67db
Prof Edir <ecaramba@gmail.com>
2024-08-07 20:15:58 -0300

    aula 7

---
+++ aula7.js
@@ -0,0 +1,34 @@
+// 1 2 3 -> 3 maior e 1 menor
+// 1 3 2
+// 2 1 3
+// 2 3 1
+// 3 1 2
+// 3 2 1
+
+function ex24()
+{
+    var n1 = prompt("digite o num1"); // 2
+    var n2 = prompt("digite o num2"); // 1
+    var n3 = prompt("digite o num3"); // 3
+
+    if (n1 > n2)
+    {
+        if (n1 > n3)
+        {
+            console.log("O maior numero é: " + n1);
+        }
+        else 
+        {
+            console.log("O maior numero é: " + n3);
+        }
+    } else 
+    {
+        if (n2 > n3)
+        {
+            console.log("O maior numero é: " + n2);
+        } else 
+        {
+            console.log("O maior numero é: " + n3);
+        }
+    }
+}
