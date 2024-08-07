4cec2780cec314a4cc106c3e31ea216f1f4a97c9
Prof Edir <ecaramba@gmail.com>
2024-08-06 21:50:12 -0300

    logica

--- aula6.js
+++ aula6.js
@@ -37,3 +37,71 @@ function ex3()
 
     saida.value = letra.toUpperCase() + resto.toLowerCase();
 }
+
+// soma os num1 + num2
+function ex4() 
+{
+    var n1 = num1.value;
+    var n2 = num2.value;
+
+    saida.value = parseInt(n1) + parseInt(n2);
+}
+
+// calculo imc
+// pedir peso e altura
+// IMC = peso/(altura x altura)
+// + soma
+// - subtração
+// / divisão
+// * multiplicação
+function ex5() 
+{
+    var n1 = num1.value;
+    var n2 = num2.value;
+
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+
+    var res = n1 / (n2 * n2);
+
+    saida.value = res;
+}
+
+// ex6
+// pegue 2 numeros e diga qual deles é o maior
+function ex6() 
+{
+    var n1 = num1.value;
+    var n2 = num2.value;
+
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+
+    if (n1 > n2)
+    {
+        saida.value = n1;
+    } else
+    {
+        saida.value = n2;
+    }
+}
+
+// ex7
+// pegue a idade e diga se é criança, adulto ou idoso
+// adulto > 18 anos, idoso > 65
+function ex7() 
+{
+    var idade = valor.value;
+
+    if (idade >= 65) 
+    {
+        saida.value = "Idoso";
+    } else if (idade >= 18)
+    {
+        saida.value = "Adulto";   
+    } else 
+    {
+        saida.value = "Criança"; 
+    }
+
+}
